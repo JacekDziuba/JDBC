3c4c4f69bedb27c74bd3775f1de0dfcf19555a82
jacek <jaco04@wp.pl>
2019-01-22 21:34:05 +0100

    JDBC

---
+++ src/Sql/Model/Artist.java
@@ -0,0 +1,27 @@
+package Sql.Model;
+
+public class Artist {
+
+    // == fields ==
+
+    private int id;
+    private String name;
+
+    // == getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
